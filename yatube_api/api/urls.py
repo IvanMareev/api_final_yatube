24361a7c8d5ac0b531a159d88111fb685e11bd5f
иван мареев <IvanMareev>
2024-11-26 23:24:25 +1000

    start integration jwt

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.authtoken import views
+from rest_framework.routers import SimpleRouter
+
+router = SimpleRouter()
 
 urlpatterns = [
+    path('', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('', include('djoser.urls.jwt')),
+    path('', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,8 @@
-# TODO:  Напишите свой вариант
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+
+
+class PostViewSet(viewsets.ViewSet):
+    pass
